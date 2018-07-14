cdc4e1cc313f2a66d4c1d59c4b5c269f3da01525
igeagonz <igea.17@gmail.com>
2018-07-14 15:53:31 -0500

    Added profile and user models along with routes

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,5 @@
 module.exports = {
-  mongoURI: 'mongodb://ignacio:ignacio1@ds137611.mlab.com:37611/devconnector-ig'
+  mongoURI:
+    'mongodb://ignacio:ignacio1@ds137611.mlab.com:37611/devconnector-ig',
+  secretOrKey: 'secret'
 };
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
 
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
@@ -7,7 +9,15 @@ const posts = require('./routes/api/posts');
 
 const app = express();
 
-// DB Config 
+// Body parser middleware
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
+app.use(bodyParser.json());
+
+// DB Config
 const db = require('./config/keys').mongoURI;
 
 // Connect to MongoDB
@@ -16,7 +26,11 @@ mongoose
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));
 
-app.get('/', (req, res) => res.send('Hello world!'));
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport Config
+require('./config/passport')(passport);
 
 // Use Routes
 app.use('/api/users', users);
