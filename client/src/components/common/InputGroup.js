b4dda38ad09ffae4d4bce12d6d1822da1765d275
igeagonz <igea.17@gmail.com>
2018-07-17 19:55:14 -0500

    Added Create Profile logic

--- client/src/components/common/InputGroup.js
+++ client/src/components/common/InputGroup.js
@@ -2,10 +2,23 @@ import React from 'react';
 import classnames from 'classnames';
 import PropTypes from 'prop-types';
 
-const InputGroup = ({ name, placeholder, value, error, info, onChange }) => {
+const InputGroup = ({
+  name,
+  placeholder,
+  value,
+  error,
+  icon,
+  type,
+  onChange
+}) => {
   return (
-    <div className="form-group">
-      <textarea
+    <div className="input-group mb-3">
+      <div className="input-group-prepend">
+        <span className="input-group-text">
+          <i className={icon} />
+        </span>
+      </div>
+      <input
         className={classnames('form-control form-control-lg', {
           'is-invalid': error
         })}
@@ -14,7 +27,6 @@ const InputGroup = ({ name, placeholder, value, error, info, onChange }) => {
         value={value}
         onChange={onChange}
       />
-      {info && <small className="form-text text-muted">{info}</small>}
       {error && <div className="invalid-feedback">{error}</div>}
     </div>
   );
@@ -24,9 +36,14 @@ InputGroup.propTypes = {
   name: PropTypes.string.isRequired,
   placeholder: PropTypes.string,
   value: PropTypes.string.isRequired,
-  info: PropTypes.string,
+  icon: PropTypes.string,
   error: PropTypes.string,
+  type: PropTypes.string.isRequired,
   onChange: PropTypes.func.isRequired
+};
+
+InputGroup.defaultProps = {
+  type: 'text'
 };
 
 export default InputGroup;
