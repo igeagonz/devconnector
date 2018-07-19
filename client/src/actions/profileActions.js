30651c18ffe058d8a1685f4b9d8b0d9362e1cdee
Ignacio Gea <ignacio.gea@cerner.com>
2018-07-19 14:05:33 -0500

    Initial setup of developers page with all profiles

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -3,6 +3,7 @@ import { logoutUser } from '../actions/authActions';
 
 import {
   GET_PROFILE,
+  GET_PROFILES,
   PROFILE_LOADING,
   CLEAR_CURRENT_PROFILE,
   GET_ERRORS
@@ -98,6 +99,25 @@ export const deleteEducation = id => dispatch => {
       dispatch({
         type: GET_ERRORS,
         payload: err.response.data
+      })
+    );
+};
+
+// Get all profiles
+export const getProfiles = () => dispatch => {
+  dispatch(setProfileLoading());
+  axios
+    .get('/api/profile/all')
+    .then(res =>
+      dispatch({
+        type: GET_PROFILES,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_PROFILES,
+        payload: null
       })
     );
 };
