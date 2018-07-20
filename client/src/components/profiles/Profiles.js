03cfdd8ecf7a30d5ca753ffef9a28c657f9b58df
igeagonz <igea.17@gmail.com>
2018-07-19 23:18:49 -0500

    Added post feed section

--- client/src/components/profiles/Profiles.js
+++ client/src/components/profiles/Profiles.js
@@ -1,25 +1,27 @@
 import React, { Component } from 'react';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import Spinner from '../common/Spinner';
+import ProfileItem from './ProfileItem';
 import { getProfiles } from '../../actions/profileActions';
 
 class Profiles extends Component {
-
   componentDidMount() {
     this.props.getProfiles();
   }
   render() {
-    const {profiles, loading} = this.props.profile;
+    const { profiles, loading } = this.props.profile;
     let profileItems;
 
-    if(profiles === null || loading) {
-      profileItems = <Spinner />
+    if (profiles === null || loading) {
+      profileItems = <Spinner />;
     } else {
-      if (profiles.length > 0){
-        profileItems = <h1>PROFILES HERE</h1>
+      if (profiles.length > 0) {
+        profileItems = profiles.map(profile => (
+          <ProfileItem key={profile._id} profile={profile} />
+        ));
       } else {
-        profileItems = <h4>No profiles found...</h4>
+        profileItems = <h4>No profiles found...</h4>;
       }
     }
 
@@ -37,17 +39,20 @@ class Profiles extends Component {
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
 Profiles.propTypes = {
   getProfiles: PropTypes.func.isRequired,
   profile: PropTypes.object.isRequired
-}
+};
 
 const mapStateToProps = state => ({
   profile: state.profile
 });
 
-export default connect(mapStateToProps, {getProfiles})(Profiles);
+export default connect(
+  mapStateToProps,
+  { getProfiles }
+)(Profiles);
